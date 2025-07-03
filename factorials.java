9b15f044d9dc6938ad68d46870479e5219138307
lambchops9 <kansarashashwat@gmail.com>
2025-07-03 17:33:09 +0700

    Create factorials.java

---
+++ factorials.java
@@ -0,0 +1,15 @@
+public class factorials {
+    public static void main(String[]args) {
+        int res = factorial(30);
+        System.out.println(res);
+    }
+
+    // recursive statement including base case
+    private static int factorial(int n) {
+        if (n == 0) {                    // base case
+            return 1;
+        } else {
+            return n * factorial(n-1);      // continues/recursive case until the base case is met
+        }
+    }
+}
